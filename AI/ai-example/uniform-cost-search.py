4d6e3c2926eb345bd2ad8b0aeb7f6d236a4557c0
mikiya <spacelion121319@gmail.com>
2023-02-05 05:55:30 -0500

    update

---
+++ AI/ai-example/uniform-cost-search.py
@@ -0,0 +1,112 @@
+
+
+# uniform cost search 
+class Graph:
+    def __init__(self):
+        self.edges = {}         # create dictionary for storing path and weight
+        self.weights = {}
+
+    # return city value with key-mapping in edges dictionary 
+    def neighbors(self, node):
+        return self.edges[node]
+
+    # return weight value with key-mapping in weights dictionary 
+    def get_cost(self, from_node, to_node):
+        return self.weights[(from_node + to_node)]
+
+    # auto build a map and weight dictionary based on Romania map
+    def auto_path(self):
+        # didn't include cities after Bucharest right now
+        self.edges = {
+            'Arad': ['Zerind', 'Sibiu', 'Timisoara'],
+            'Zerind': ['Oradea'],
+            'Oradea': ['Sibiu'],
+            'Sibiu': ['Fagaras', 'Rimnicu Vilcea'],
+            'Fagaras': ['Bucharest'],
+            'Rimnicu Vilcea': ['Pitesti', 'Craiova'],
+            'Craiova': ['Pitesti'],
+            'Pitesti': ['Bucharest'],
+            'Bucharest': [],
+
+            'Timisoara': ['Lugoj'],
+            'Lugoj': ['Mehadia'],
+            'Mehadia': ['Dobreta'],
+            'Dobreta': ['Craiova']
+        }
+        # distance use int value
+        self.weights = {
+            'AradZerind': 75,
+            'AradSibiu': 140,
+            'AradTimisoara': 118,
+            'ZerindOradea': 71,
+            'OradeaSibiu': 151,
+            'SibiuFagaras': 99,
+            'FagarasBucharest': 211,
+            'SibiuRimnicu Vilcea': 80,
+            'Rimnicu VilceaPitesti': 97,
+            'PitestiBucharest': 101,
+            'Rimnicu VilceaCraiova': 146,
+            'CraiovaPitesti': 138,
+
+            'TimisoaraLugoj': 111,
+            'LugojMehadia': 70,
+            'MehadiaDobreta': 75,
+            'DobretaCraiova': 120
+        }
+
+    # prompt use to enter nodes and weights 
+    def addVertex(self):
+        edge = input("Enter a node: ")
+        self.edges[edge] = []
+        weight = input("Enter a weight: ")
+        self.weights[edge] = [weight]
+
+    # clean up edges and weights 
+    def empty(self):
+        self.edges = {} 
+        self.weights = {}
+
+
+#
+g = Graph() 
+# g.prompt()
+# g.prompt()
+g.auto_path()
+
+# greedy algorithm: uniform cost search 
+# make locally optimal choice at each step 
+# meaning: choose the next vertex to visit based on the minimum cost among nodes at the current level 
+# not necessarily get the shortest path globally, but shortest path for every two nodes 
+
+def ucs(graph, start, end):
+    queue = [start]
+    visited = [start]
+    total = 0
+
+    while queue:
+        vertex = queue.pop(0)
+        if vertex == end:
+            # print(visited)
+            # print(queue)
+            print(f'total distance: {total}')
+            return total
+        
+        result = float('inf')
+        holder = ''
+        # for num, node in enumerate(graph.edges[vertex]):
+        for node in graph.edges[vertex]:
+            weight = graph.get_cost(vertex, node)
+            if weight < result:
+                result = weight
+                holder = node
+                print(holder)
+        if holder not in visited:
+            visited.append(holder) 
+            queue.append(holder)
+            print(f"{vertex} -> {holder}: {result}")
+            total += result
+    return 
+
+print(f"shortest distance based on Uniform Cost Search: {ucs(g, 'Arad', 'Bucharest')}")
+
+
--- README.md
+++ README.md
@@ -4,17 +4,18 @@
 do data science work here
 ```
 
-## &#x237e; Python Basic
-##### &#x23f5; [packages](./basic/python_package.md)
-##### &#x23f5; [IDE](./basic/ide.md)
+## &#x237e; Python
+##### &#x23f5; Necessity: [IDE](./basic/ide.md) | [Packages](./basic/python_package.md)
 <br />
 
 ## &#x237e; Machine Learning
-##### &#x23f5; [markov chain](./ML/markov_chain)
+##### &#x23f5; Example: [parts](./ML/ml-example/)
+##### &#x23f5; Model: [markov chain](./ML/Model/markov_chain/)
+##### &#x23f5; DL: [neural network](./DL/neural_network)
 <br />
 
-## &#x237e; Deep Learning
-##### &#x23f5; [neural network](./DL/neural_network)
+## &#x237e; AI
+##### &#x23f5; Instance: [parts](./AI/ai-example/)
 <br />
 
 # &#x232d; Mathematics
@@ -26,6 +27,9 @@ math is important almost for everything
 ##### &#x23f5; [Linear Algebra](./math/Linear_Algebra)
 <br />
 
+## &#x237e; Statistics
+##### &#x23f5; [Probability](./math/Statistics/probability.md)
+<br />
 
 ## &#x237e; Latex
 ##### &#x23f5; [guide](./math/LaTex/LatexGuide.md) | [templates](./math/LaTex/templates/)
